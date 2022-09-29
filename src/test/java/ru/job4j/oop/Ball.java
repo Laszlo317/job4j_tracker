e12f7c3c2b7dcbd013308396ab1933c6b57c6de3
Laszlo317 <iwannabethecoder413@gmail.com>
2022-09-29 11:11:42 +0300

    1.6. Взаимодействие объектов [#166087]

---
+++ src/test/java/ru/job4j/oop/Ball.java
@@ -0,0 +1,11 @@
+package ru.job4j.oop;
+
+public class Ball {
+    public void tryRun(boolean isGulped) {
+        if (isGulped) {
+            System.out.println("Oh no! They ate Ball!");
+        } else {
+            System.out.println("Phew...");
+        }
+    }
+}
