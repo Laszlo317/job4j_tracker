41a849d8f97ae188303f2cdbab1d57ef7a6e4c7a
Laszlo317 <iwannabethecoder413@gmail.com>
2022-10-04 15:54:39 +0300

    9.2. Тесты вывода на консоль [#33585 #347736]

--- src/main/java/ru/job4j/tracker/ReplaceAction.java
+++ src/main/java/ru/job4j/tracker/ReplaceAction.java
@@ -2,9 +2,9 @@ package ru.job4j.tracker;
 
 public class ReplaceAction implements UserAction {
 
-    Output out;
+    private Output out;
 
-    ReplaceAction(Output out) {
+    public ReplaceAction(Output out) {
         this.out = out;
     }
 
