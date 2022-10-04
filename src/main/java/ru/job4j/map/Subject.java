5d601a13b617dd6b44bbf0acd0e55ac96710241d
Laszlo317 <iwannabethecoder413@gmail.com>
2022-10-04 11:08:15 +0300

    4. Аттестация [#504878 #348544]

--- src/main/java/ru/job4j/map/Subject.java
+++ src/main/java/ru/job4j/map/Subject.java
@@ -1,18 +1,47 @@
 package ru.job4j.map;
 
-public record Subject(String name, int score) {
+import java.util.Objects;
+
+public final class Subject {
+    private final String name;
+    private final int score;
+
     public Subject(String name, int score) {
         this.name = name;
         this.score = score;
     }
 
-    @Override
     public String name() {
         return name;
     }
 
-    @Override
     public int score() {
         return score;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+        var that = (Subject) obj;
+        return Objects.equals(this.name, that.name)
+                && this.score == that.score;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, score);
+    }
+
+    @Override
+    public String toString() {
+        return "Subject["
+                + "name=" + name + ", "
+                + "score=" + score + ']';
+    }
+
 }
