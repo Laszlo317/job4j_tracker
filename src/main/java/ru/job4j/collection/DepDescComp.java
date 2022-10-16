9c241a6db5a6d1cf58d896b8989247c174ca8453
Laszlo317 <iwannabethecoder413@gmail.com>
2022-10-16 13:01:30 +0300

    2. Отсортировать департаменты [#34352 #352020]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -11,7 +11,7 @@ public class DepDescComp implements Comparator<String> {
         int length = Math.min(s1.length, s2.length);
         rsl = s2[0].compareTo(s1[0]);
         if (rsl == 0) {
-            for (int i = 0; i < length; i++) {
+            for (int i = 1; i < length; i++) {
                 rsl = s1[i].compareTo(s2[i]);
                 if (rsl != 0) {
                     break;
